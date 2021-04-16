1d8bae396e8f4f0119040f5c33f1ed711d77c523
taiga-kishimoto-2233 <taiga.kishimoto.rf@gmail.com>
2021-04-16 14:17:59 +0900

    商品情報編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :move_to_index, except: [:index, :show]
+
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -22,10 +24,36 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :text, :category_id, :states_id, :charge_id, :area_id, :exhibit_date_id,
                                  :price, :image).merge(user_id: current_user.id)
+  end
+  def move_to_index
+    # if @purchases.item.present?
+    #   redirect_to action: :index
+    # else
+      if user_signed_in?
+        if !(current_user.items.find_by(id: params[:id]))
+          redirect_to action: :index
+        end
+      else
+        redirect_to new_user_session_path
+      end
+    # end
   end
 end
