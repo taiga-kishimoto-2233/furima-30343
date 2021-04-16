7614d7e7d4a10740c0729c3cdd8e0503521bf30a
taiga-kishimoto-2233 <taiga.kishimoto.rf@gmail.com>
2021-04-16 14:19:40 +0900

    省情報編集 #1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   before_action :move_to_index, except: [:index, :show]
 
-
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -43,17 +42,16 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :text, :category_id, :states_id, :charge_id, :area_id, :exhibit_date_id,
                                  :price, :image).merge(user_id: current_user.id)
   end
+
   def move_to_index
     # if @purchases.item.present?
     #   redirect_to action: :index
     # else
-      if user_signed_in?
-        if !(current_user.items.find_by(id: params[:id]))
-          redirect_to action: :index
-        end
-      else
-        redirect_to new_user_session_path
-      end
+    if user_signed_in?
+      redirect_to action: :index unless current_user.items.find_by(id: params[:id])
+    else
+      redirect_to new_user_session_path
+    end
     # end
   end
 end
