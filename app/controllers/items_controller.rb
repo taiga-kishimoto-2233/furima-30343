7125efddc9148748358a367e5266fba957b5bb4a
taiga-kishimoto-2233 <taiga.kishimoto.rf@gmail.com>
2021-04-16 17:11:56 +0900

    商品情報編集 #2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :move_to_index, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -20,17 +21,14 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to root_path
+      redirect_to item_path
     else
       render :edit
     end
@@ -43,15 +41,15 @@ class ItemsController < ApplicationController
                                  :price, :image).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def move_to_index
     # if @purchases.item.present?
     #   redirect_to action: :index
     # else
-    if user_signed_in?
-      redirect_to action: :index unless current_user.items.find_by(id: params[:id])
-    else
-      redirect_to new_user_session_path
-    end
+    redirect_to action: :index unless current_user.items.find_by(id: params[:id])
     # end
   end
 end
