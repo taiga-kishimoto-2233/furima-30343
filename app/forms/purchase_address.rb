6362d41dbd86efcbbb935d58c32f1f7ea39f8890
taiga-kishimoto-2233 <taiga.kishimoto.rf@gmail.com>
2021-04-29 13:38:41 +0900

    Update purchase_address.rb

--- app/forms/purchase_address.rb
+++ app/forms/purchase_address.rb
@@ -14,11 +14,9 @@ class PurchaseAddress
     validates :user_id,
               :item_id,
               :token
-    validates :postal_code,
-              :area_id,
+    validates :area_id,
               :municipality,
-              :address,
-              :phone_number
+              :address
     validates :area_id, numericality: { other_than: 1 }
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
